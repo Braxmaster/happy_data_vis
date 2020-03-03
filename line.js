a2968c73007f1a0ae25906cb2a7f8401ab06c664
Glitch (braxmaster-happy-data-vis) <none>
2020-03-03 10:25:14 +0000

    👳😼 Checkpoint ./line.js:10543347/1720:12024331/1

--- line.js
+++ line.js
@@ -5,7 +5,7 @@ var chart = new Chart(ctx, {
 
     // The data for our dataset
     data: {
-        labels: ['$5 000', '$10 000', '$20 000', '$40 000', '$80 000', '$95 000', '$160 000'],
+        labels: ['$5 000', '$10 000', '$20 000', '$40 000', '$80 000', '$95 000', '$100 000', '$160 000'],
 
         
         datasets: [{
@@ -16,13 +16,7 @@ var chart = new Chart(ctx, {
             borderColor: 'rgb(177, 233, 245)',
             backgroundColor: 'rgb(177, 233, 245)',
           
-            data: [{
-              label: 'Global',
-              data: [5.1, 5.75, 6.5, 7.2, 7.65, 7.8, 7.2]},
-                   {
-                     label: 'Western Europe/Scandinavia',
-                     data: [6.1, 6.4, 6.7, ],
-                   }/*Data points that maps an annual income value to a happiness index */
+            data: [5.1, 5.75, 6.5, 7.2, 7.65, 7.8, 7.65, 7.2],/*Data points that maps an annual income value to a happiness index */
             
           
             pointRadius: 5,
@@ -33,6 +27,22 @@ var chart = new Chart(ctx, {
             /*steppedLine: true,*/ 
     
 
+        }, {
+          label: 'Western Europe/Scandinavia',
+            fill: false,    
+            pointBorderColor: ['rgb(41, 158, 184)','rgb(41, 158, 184)','rgb(41, 158, 184)','rgb(41, 158, 184)', 'rgb(41, 158, 184)','rgb(255,192,203)','rgb(41, 158, 184)'],
+            pointBackgroundColor: ['rgb(41, 158, 184)','rgb(41, 158, 184)','rgb(41, 158, 184)','rgb(41, 158, 184)','rgb(41, 158, 184)','rgb(255,192,203)','rgb(41, 158, 184)'],
+            borderColor: 'rgb(177, 233, 245)',
+            backgroundColor: 'rgb(177, 233, 245)',
+          
+            data: [6.1, 6.4, 6.7, 7.4, 7.6, 7.75 ,7.8, 7.5], /*Data points that maps an annual income value to a happiness index */
+            
+          
+            pointRadius: 5,
+            pointStyle: ['circle','circle','circle','circle', 'circle', 'rect','circle'],
+            pointBorderWidth: 4,
+            borderWidth: 2,
+            hoverBorderWidth: 10,
         }]
     },
 
